8f385c2e97873b699d0af6f1a5d62b7424630fcd
Tristan John Potgieter <34542710+Divergent-Caesar@users.noreply.github.com>
2024-09-04 22:11:53 +0200

    other stuff?

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+112cbb930d599d762db63cd1899782b7e5a4a53e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496dfc02e5c2ff6237abc4dac8097ec17b3b5252")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
